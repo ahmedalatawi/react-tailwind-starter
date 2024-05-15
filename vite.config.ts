1418dd1e4b4764a60005ac95765e4a613aee3dd3
Ahmed Alatawi <aalat001@gmail.com>
2024-05-15 16:14:56 -0400

    Configured TS checker and changed localhost port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+    // If you'd prefer to not run the checkers during unit testing with Vitest, you can alter the config based on that. Example:
+    // !process.env.VITEST ? checker({ typescript: true }) : undefined
+  ],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+});
