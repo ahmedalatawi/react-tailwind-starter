ef6569b50727506adf9a13c86273e88cebad2f15
Ahmed Alatawi <aalat001@gmail.com>
2024-05-15 17:17:54 -0400

    Configured eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+//@ts-ignore
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    eslint(),
     checker({
       // e.g. use TypeScript check
       typescript: true,
