f664d88818137a74f7b94a60ba661ff8c8c552f3
Ahmed Alatawi <aalat001@gmail.com>
2024-05-17 12:42:07 -0400

    Added util helpers and Cart related components

---
+++ src/pages/cart/CartItem.tsx
@@ -0,0 +1,22 @@
+import { formatCurrency } from '../../utils/helpers';
+
+interface Props {
+  item: any;
+}
+
+function CartItem({ item }: Props) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
